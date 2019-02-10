f789723107e6127f075182edf3ddb6bdb11d495d
Emile Violi <31874169+evioli@users.noreply.github.com>
2019-02-11 09:55:05 +1100

    First working version

---
+++ counter/src/index.js
@@ -0,0 +1,48 @@
+import h from 'hyperscript';
+import hh from 'hyperscript-helpers';
+
+const { div, button } = hh(h);
+
+const initModel = 0;
+
+function view(dispatch, model) {
+  return div([
+    div({ className: 'mv2' }, `Count: ${model}`),
+    button({ className: 'pv1 ph2 mr2',
+      onclick: () => dispatch('plus') }, '+'),
+    button({ className: 'pv1 ph2',
+      onclick: () => dispatch('minus') }, '-'),
+  ]);
+}
+
+function update(msg, model) {
+  switch (msg) {
+    case 'plus':
+      return model + 1;
+    case 'minus':
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+// impure code below
+
+function app(initModel, update, view, node) {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  node.appendChild(currentView);
+
+  function dispatch(msg) {
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    node.replaceChild(updatedView, currentView);
+    currentView = updatedView;
+  };
+}
+
+const rootNode = document.getElementById('app');
+
+app(initModel, update, view, rootNode);
+
+// rootNode.appendChild(view(update('minus', initModel)));
