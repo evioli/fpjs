2a0d8c76e669d447f71c0a64559794aca8c4fc0d
Emile Violi <31874169+evioli@users.noreply.github.com>
2019-02-20 09:20:43 +1100

    wire up upddate functionality

--- tempunitconvert_starter/src/Update.js
+++ tempunitconvert_starter/src/Update.js
@@ -1,6 +1,64 @@
 import * as R from 'ramda';
 
+const MSGS = {
+  LEFT_VALUE: 'LEFT_VALUE',
+  RIGHT_VALUE: 'RIGHT_VALUE',
+  LEFT_UNIT: 'LEFT_UNIT',
+  RIGHT_UNIT: 'RIGHT_UNIT',
+}
+
+export function updateLeftValueMsg(leftValue) {
+  return {
+    type: MSGS.LEFT_VALUE,
+    leftValue,
+  };
+}
+
+export function updateRightValueMsg(rightValue) {
+  return {
+    type: MSGS.RIGHT_VALUE,
+    rightValue,
+  };
+}
+
+export function updateLeftUnitMsg(leftUnit) {
+  return {
+    type: MSGS.LEFT_UNIT,
+    leftUnit,
+  };
+}
+
+export function updateRightUnitMsg(rightUnit) {
+  return {
+    type: MSGS.RIGHT_UNIT,
+    rightUnit,
+  };
+}
+
 function update (msg, model) {
+  switch(msg.type){
+    case MSGS.LEFT_VALUE: {
+      if(msg.leftValue === '')
+        return { ...model, sourceLeft: true, leftValue: '', rightValue: '' };
+      const leftValue = toInt(msg.leftValue);
+      if(model.leftValue ===)
+      return { ...model, sourceLeft: true, leftValue }
+    }
+    case MSGS.RIGHT_VALUE: {
+      if(msg.rightValue === '')
+        return { ...model, sourceLeft: false, leftValue: '', rightValue: '' };
+      const rightValue = toInt(msg.rightValue);
+      return { ...model, sourceLeft: false, rightValue }
+    }
+    case MSGS.LEFT_UNIT: {
+      const { leftUnit } = msg;
+      return { ...model, leftUnit }
+    }
+    case MSGS.RIGHT_UNIT: {
+      const { rightUnit } = msg;
+      return { ...model, rightUnit }
+    }
+  }
   return model;
 }
 
--- tempunitconvert_starter/src/View.js
+++ tempunitconvert_starter/src/View.js
@@ -1,6 +1,12 @@
 import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import {
+  updateLeftValueMsg,
+  updateRightValueMsg,
+  updateLeftUnitMsg,
+  updateRightUnitMsg,
+} from './Update';
 
 const {
   div,
@@ -20,16 +26,18 @@ function unitOptions(selectedUnit) {
   )
 }
 
-function unitSection(dispatch, unit, value) {
+function unitSection(dispatch, unit, value, inputMsg, dropdownMsg) {
   return div({ className: 'w-50 ma1' }, [
     input({
       className: 'db w-100 mv2 pa2 input-reset ba',
       type: 'text',
       value,
+      oninput: e => dispatch(inputMsg(e.target.value))
     }),
     select(
       {
         className: 'db w-100 pa2 input-reset br1 bg-white ba b--black',
+        onchange: e => dispatch(dropdownMsg(e.target.value))
       },
       unitOptions(unit),
     ),
@@ -43,11 +51,15 @@ function view(dispatch, model) {
       unitSection(dispatch,
         model.leftUnit,
         model.leftValue,
+        updateLeftValueMsg,
+        updateLeftUnitMsg
         ),
         div({ className: 'pa2 tc'},'='),
       unitSection(dispatch,
         model.rightUnit,
         model.rightValue,
+        updateRightValueMsg,
+        updateRightUnitMsg
         ),
     ]),
     pre(JSON.stringify(model, null, 2)),
