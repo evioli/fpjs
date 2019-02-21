42210a195c804ffc130e07dd91d383da4745c8a1
Emile Violi <31874169+evioli@users.noreply.github.com>
2019-02-21 15:37:01 +1100

    my attempt

--- tempunitconvert_starter/src/Update.js
+++ tempunitconvert_starter/src/Update.js
@@ -38,17 +38,40 @@ export function updateRightUnitMsg(rightUnit) {
 function update (msg, model) {
   switch(msg.type){
     case MSGS.LEFT_VALUE: {
-      if(msg.leftValue === '')
+      if(msg.leftValue === '') {
         return { ...model, sourceLeft: true, leftValue: '', rightValue: '' };
-      const leftValue = toInt(msg.leftValue);
-      if(model.leftValue ===)
-      return { ...model, sourceLeft: true, leftValue }
+      }
+
+      const leftValue = R.pipe(
+        parseFloat,
+        R.defaultTo(''),
+      )(msg.leftValue);
+
+      if(model.leftUnit === model.rightUnit){
+        return { ...model, sourceLeft: true, leftValue, rightValue: leftValue };
+      }
+
+      const rightValue = convertTemp(model);
+
+      return { ...model, sourceLeft: true, leftValue, rightValue };
     }
     case MSGS.RIGHT_VALUE: {
-      if(msg.rightValue === '')
+      if(msg.rightValue === '') {
         return { ...model, sourceLeft: false, leftValue: '', rightValue: '' };
-      const rightValue = toInt(msg.rightValue);
-      return { ...model, sourceLeft: false, rightValue }
+      }
+      const rightValue = R.pipe(
+        parseInt,
+        R.defaultTo(''),
+      )(msg.rightValue);
+
+      if(model.leftUnit === model.rightUnit)
+      {
+        return { ...model, sourceLeft: false, leftValue: rightValue, rightValue }
+      }
+
+      const leftValue = convertTemp(model);
+
+      return { ...model, sourceLeft: false, leftValue, rightValue }
     }
     case MSGS.LEFT_UNIT: {
       const { leftUnit } = msg;
@@ -60,6 +83,110 @@ function update (msg, model) {
     }
   }
   return model;
+}
+
+function convertTemp(model)
+{
+  if(model.sourceLeft) {
+    switch(model.leftUnit)
+    {
+      case 'Fahrenheit': {
+        switch(model.rightUnit) {
+          case 'Celsius': {
+            return fahrenheitToCelsius(model.leftValue);
+          }
+          case 'Kelvin': {
+            return R.pipe(fahrenheitToCelsius(model.leftValue), celsiusToKelvin());
+          }
+        }
+      }
+
+      case 'Celsius': {
+        switch(model.rightUnit)
+        {
+          case 'Fahrenheit': {
+            return celsiusToFahernheit(model.leftValue);
+          }
+          case 'Kelvin': {
+            return celsiusToKelvin(model.leftValue);
+          }
+        }
+      }
+
+      case 'Kelvin': {
+        switch(model.rightUnit) {
+          case 'Fahrenheit': {
+            return R.pipe(kelvinToCelsius(model.leftValue), celsiusToFahernheit());
+          }
+          case 'Celsius': {
+            return kelvinToCelsius(model.leftValue);
+          }
+        }
+      }
+    }
+    return model.leftValue;
+  }
+  else{
+    switch(model.rightUnit)
+    {
+      case 'Fahrenheit': {
+        switch(model.leftUnit) {
+          case 'Celsius': {
+            return fahrenheitToCelsius(model.rightValue);
+          }
+          case 'Kelvin': {
+            return R.pipe(fahrenheitToCelsius(model.rightValue), celsiusToKelvin());
+          }
+        }
+      }
+
+      case 'Celsius': {
+        switch(model.leftUnit)
+        {
+          case 'Fahrenheit': {
+            return celsiusToFahernheit(model.rightValue);
+          }
+          case 'Kelvin': {
+            return celsiusToKelvin(model.rightValue);
+          }
+        }
+      }
+
+      case 'Kelvin': {
+        switch(model.leftUnit) {
+          case 'Fahrenheit': {
+            return R.pipe(kelvinToCelsius(model.rightValue), celsiusToFahernheit());
+          }
+          case 'Celsius': {
+            return kelvinToCelsius(model.rightValue);
+          }
+        }
+      }
+    }
+    return model.rightValue;
+  }
+}
+
+const convertLeftFahrenheit = R.cond([
+  [R.equals('Celsius'), temp => fahrenheitToCelsius(temp) ],
+  [R.equals('Kelvin'), temp => R.pipe(fahrenheitToCelsius(temp), celsiusToKelvin())],
+  [R.T, temp => temp],
+]);
+
+function fahrenheitToCelsius(temperature){
+  return (5/9) * (temperature - 32.0);
+}
+
+function celsiusToFahernheit(temperature){
+  return (9/5) * temperature + 32.0;
+}
+
+function kelvinToCelsius(temperature){
+  return temperature - 273.15;
+}
+
+function celsiusToKelvin(temperature){
+  return temperature + 273.15;
 }
 
 export default update;
