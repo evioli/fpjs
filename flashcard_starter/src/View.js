8e50a38329510993f82cc1fbe00677e43e532151
Emile Violi <31874169+evioli@users.noreply.github.com>
2019-02-25 10:50:24 +1100

    create initial view

--- flashcard_starter/src/View.js
+++ flashcard_starter/src/View.js
@@ -1,12 +1,78 @@
 import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { disconnect } from 'cluster';
 
-const { div, h1 } = hh(h);
+const {
+  pre,
+  div,
+  h1,
+  button,
+  i,
+} = hh(h);
+
+function addButton(dispatch)
+{
+  return div([
+    button({
+      className: 'pa2 br1 mv2 bg-green bn white'
+    },
+    [
+      i( {
+        className: 'fa fa-plus ph1'
+      }),
+      'Add Flashcard',
+    ]),
+  ]);
+}
+
+function labelSet(title, text)
+{
+  return div([
+    div({
+      className: 'b f6 mv1 underline'
+    },
+    title,
+    ),
+    div({
+      className: 'pointer'
+    },
+    text,
+    ),
+  ]);
+}
+
+function rankSet()
+{
+  return div({
+    className: 'absolute bottom-0 left-00 w-100 ph2'
+  },
+  [
+
+  ]);
+}
+
+function flashCard(dispatch)
+{
+  return div({
+    className: 'w-third pa2'
+  },
+  [
+    div({
+      className: 'w-100 pa2 bg-light-yellow shadow-1 mv2 realtive pb5'
+    },
+    [
+      labelSet('Question', 'Question text'),
+      labelSet('Answer', 'Answer text'),
+    ]),
+  ]);
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw8 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Flashcard Study'),
+    addButton(dispatch),
+    flashCard(dispatch),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
