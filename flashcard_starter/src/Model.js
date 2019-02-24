8f9e0938032a96122f97421b1493f3ee13d4b507
Emile Violi <31874169+evioli@users.noreply.github.com>
2019-02-25 10:50:02 +1100

    create initial model

--- flashcard_starter/src/Model.js
+++ flashcard_starter/src/Model.js
@@ -1,3 +1,15 @@
-const initModel = {};
+const initModel = {
+  flashcards: [],
+  nextId: 0,
+};
+
+const flashCard = {
+  id: 0,
+  question: 'Question?',
+  answer: 'Answer',
+  editMode: false,
+  rank: 0,
+
+}
 
 export default initModel;
